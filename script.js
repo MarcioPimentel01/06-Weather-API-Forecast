ad59a99985da248cc1737dddf893ae5c0753c717
Marcio Pimentel <149727150+MarcioPimentel01@users.noreply.github.com>
2024-05-08 11:37:32 -0400

    Async fetch data function working

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 //5 days Forecast url - api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}
+//https://api.openweathermap.org/data/2.5/forecast?q=orlando&appid=15cc27ace4d4eac55708dcf5dd1ea30d"
 
-fetch("https://api.openweathermap.org/data/2.5/forecast?q=orlando&appid=15cc27ace4d4eac55708dcf5dd1ea30d")
-    .then(response => response.json())
-    .then(data => console.log(data.city))
-    .catch(error => console.log(error))
+fetchData()
 
+async function fetchData() {
+    try {
+        const response = await fetch("https://api.openweathermap.org/data/2.5/forecast?q=orlando&appid=15cc27ace4d4eac55708dcf5dd1ea30d")
+
+            if (!response.ok) {
+                throw new console.error('Unable to fetch information at this moment');
+            }
+
+            const data = await response.json()
+            console.log(data)
+
+    } catch (error) {
+        console.error(error)
+    }
+}
 
