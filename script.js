5b3be54fd32dcd36870013feed53b433b4b4309a
Marcio Pimentel <149727150+MarcioPimentel01@users.noreply.github.com>
2024-05-08 14:21:45 -0400

    uhull fetching the cities is functional

--- script.js
+++ script.js
@@ -4,6 +4,9 @@
 const APIKey = "15cc27ace4d4eac55708dcf5dd1ea30d"
 const _btnSearch = document.getElementById(`btnSearch`)
 const _dropdownSearch = document.querySelectorAll(`.dropdown-item`)
+const _inputSearch = document.getElementById(`inputSearch`)
+
+const cities = []
 
 // ==============================================================================================
 //                             Event Listeners
@@ -13,31 +16,31 @@ document.addEventListener(`DOMContentLoaded`, (ev) => {
     ev.preventDefault()
     
     _btnSearch.addEventListener(`click`, () => {
-         console.log(`ok`)
-        let _inputSearch = document.getElementById(`inputSearch`).value
+        const _inputSearch = document.getElementById(`inputSearch`).value.trim()
         console.log(_inputSearch)
+        cities.unshift(_inputSearch)
+        fetchData()
     })
-})
-
-_dropdownSearch.forEach(itemSearch => { //forEach was used because I'm using a querySelectoAll to targer multiple items
-    itemSearch.addEventListener(`click`, () => {
-        console.log(`ok`)
+    
+    _dropdownSearch.forEach(itemSearch => { //forEach was used because I'm using a querySelectoAll to targer multiple items
+        itemSearch.addEventListener(`click`, () => {
+            console.log(`ok`)
+        })
     })
-})
+    
+    
+    // fetchData()
+    
 
-
-// fetchData()
-
-
-
-async function fetchData() {
+    
+    async function fetchData() {
     try {
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${_inputSearch}&appid=${APIKey}`)
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cities[0]}&appid=${APIKey}`)
 
-            if (!response.ok) {
-                throw new console.error('Unable to fetch information at this moment');
+        if (!response.ok) {
+            throw new console.error('Unable to fetch information at this moment');
             }
-
+            
             const data = await response.json()
             console.log(data)
 
@@ -46,3 +49,5 @@ async function fetchData() {
     }
 }
 
+
+})
