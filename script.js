fb20bfd0a234d76d50529ca49ef975035ce68346
Marcio Pimentel <149727150+MarcioPimentel01@users.noreply.github.com>
2024-05-08 10:12:29 -0400

    step by step commit

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 //5 days Forecast url - api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}
 
 fetch("https://api.openweathermap.org/data/2.5/forecast?q=orlando&appid=15cc27ace4d4eac55708dcf5dd1ea30d")
-    .then(response => console.log(response))
+    .then(response => response.json())
+    .then(data => console.log(data.city))
     .catch(error => console.log(error))
 
 
